3d51f4cd7f7c30c0337efe4a268d401ce839b771
latteeea <kimtaeran4767@gmail.com>
2026-05-05 14:03:03 +0900

    feat: 일기 보관함 네비 탭 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,6 +26,17 @@ export default function TabLayout() {
         tabBarStyle: styles.tabBar,
       }}>
       <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <View style={styles.tabBarIconOffset}>
+              <NavigationTabIcon name="home" color={color} />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="write"
         options={{
           title: 'Write',
@@ -37,12 +48,12 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="index"
+        name="diary"
         options={{
-          title: 'Home',
+          title: 'Diary',
           tabBarIcon: ({ color }) => (
             <View style={styles.tabBarIconOffset}>
-              <NavigationTabIcon name="home" color={color} />
+              <NavigationTabIcon name="diary" color={color} />
             </View>
           ),
         }}
